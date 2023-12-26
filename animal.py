21a3dabf21b4cb0c271dc226d54afe155bc98331
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-12-26 12:09:03 +0000

    Принципы ООП
    - Проверка ДЗ
    - Удален ненужный файл `animals.py`

--- animal.py
+++ animal.py
@@ -2,6 +2,7 @@ import abc
 
 
 class Animal(abc.ABC):
+    """Абстрактный класс."""
 
     @abc.abstractmethod
     def head(self):
@@ -20,8 +21,12 @@ class Animal(abc.ABC):
         pass
 
 
-class Cat(Animal):
-    def __init__(self, name, breed, gender, color):
+class AnimalBase(Animal):
+    """Базовый класс животных."""
+
+    def __init__(
+            self, name: str, breed: str, gender: str, color: str
+    ) -> None:
         self.name = name
         self.breed = breed
         self.gender = gender
@@ -39,16 +44,30 @@ class Cat(Animal):
     def ears(self):
         print('Уши')
 
-    def info(self):
-        template = (f'\nМеня зовут {self.name}'
-                    f'\nПорода: {self.breed}'
-                    f'\nПол: {self.gender}')
+    def info(self) -> str:
+        template: str = (f'\nМеня зовут {self.name}'
+                         f'\nПорода: {self.breed}'
+                         f'\nПол: {self.gender}')
 
         return template
 
 
-class Dog(Cat):
-    def __init__(self, name, breed, gender, color, age):
+class Cat(AnimalBase):
+    """Класс кошек."""
+
+    def __init__(
+            self, name: str, breed: str, gender: str, color: str
+    ) -> None:
+        super().__init__(name, breed, gender, color)
+
+    def purr(self):
+        print('Мурлыкать')
+
+
+class Dog(AnimalBase):
+    def __init__(
+            self, name: str, breed: str, gender: str, color: str, age: int
+    ) -> None:
         super().__init__(name, breed, gender, color)
         self.age = age
 
@@ -57,11 +76,16 @@ class Dog(Cat):
             return 'Собака в возрасте'
         return 'молодая'
 
-    def info(self):
+    def info(self) -> str:
         template = super().info()
         template += f'\nСтарая или молодая? {self.checking_for_old_age()}'
         return template
 
 
-dog = Dog('Соня', 'Сибирская', 'девочка', 'Черная', 30)
-print(dog.info())
+if __name__ == '__main__':
+    cat_1 = Cat('Рыжик', 'A+', 'M', 'рыжий')
+    print(cat_1.info())
+    print(cat_1.purr())
+
+    dog = Dog('Соня', 'Сибирская', 'девочка', 'Черная', 30)
+    print(dog.info())
