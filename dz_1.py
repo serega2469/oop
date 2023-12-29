7f284016a8176e31aeb49f83053709f652e226ac
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-12-29 00:25:33 +0000

    Принципы ООП
    - Проверено ДЗ

--- dz_1.py
+++ dz_1.py
@@ -1,4 +1,5 @@
-class Human:
+class Human:  # Скорее всего этот класс уже не Human,
+    # а Person будет правильнее
 
     def __init__(self, name: str, age: int, many: bool | int, home: str):
         self.name = name
@@ -22,15 +23,13 @@ class Human:
 
     def buy_a_many(self):
         house_price = 5
-        if self.many:
+        if self.many:  # Проверка ни о чем не говорит, его нужно вычесть
+            # из self.many, только если у покупателя достаточно средств на счету
             self.many -= house_price
+            # Тут лучше использовать скобки для переноса
+            # текста, вместо слэша, как в 11 строке кода
             return f'\nВы купили дом стоимостью: {house_price} руб.' \
                    f' На счете осталось {self.many}'
-
-
-user_1 = Human('Cергей', 24, 10000, 'Есть')
-print(user_1.make_money())
-print(user_1.buy_a_many())
 
 
 class Home:
@@ -39,15 +38,19 @@ class Home:
         self.cost = cost
 
     def discount_home(self):
-        discount = 50
+        discount = 50  # Скидка всегда будет 50% или для всех будет разный %?
         return f'Скидка на покупку дома составляет: {self.cost / discount}'
 
 
-class New_home(Home):
+# Классы должны называться в соответствии с нотацией PascalCase.
+class New_home(Home):  # Почитать можно тут: https://skillbox.ru/media/code/notatsii-v-programmirovanii/
     def __init__(self, area, cost):
+        # Тут ошибка, забыли круглые скобки после super
         super.__init__(area, cost)
 
 
-
-
-
+if __name__ == '__main__':  # Используйте точку входа,
+    # в коде не должно создаваться никаких объектов.
+    user_1 = Human('Cергей', 24, 10000, 'Есть')
+    print(user_1.make_money())
+    print(user_1.buy_a_many())
